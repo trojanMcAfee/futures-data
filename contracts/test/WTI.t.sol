a2a4d94c2e257734fdfd87ff05f9b0cdcb83bf78
dnyrm <cdgmachado0@gmail.com>
2025-02-24 16:24:24 +0100

    Added arbitrum price query

--- contracts/test/WTI.t.sol
+++ contracts/test/WTI.t.sol
@@ -3,10 +3,14 @@ pragma solidity ^0.8.20;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {WTI} from "../src/WTI.sol";
+import {AggregatorV3Interface} from "../src/interfaces/AggregatorV3Interface.sol";
 
 contract WTITest is Test {
     WTI public wti;
     address public deployer;
+    
+    // WTI/USD Chainlink Data Feed on Arbitrum
+    address constant CHAINLINK_WTI_USD = 0x594b919AD828e693B935705c3F816221729E7AE8;
     
     function setUp() public {
         // Create a new account for the deployer
@@ -37,5 +41,37 @@ contract WTITest is Test {
         assertEq(wti.name(), "Crude Oil", "Token name should be 'Crude Oil'");
         assertEq(wti.symbol(), "WTI", "Token symbol should be 'WTI'");
         assertEq(wti.decimals(), 18, "Token should have 18 decimals");
+    }
+
+    function test_ChainlinkWTIPrice() public {
+        // Fork Arbitrum at specific block
+        vm.createSelectFork(vm.envString("ARBITRUM_RPC_URL"), 309442764);
+        
+        // Get the Chainlink price feed
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(CHAINLINK_WTI_USD);
+        
+        // Get the latest price data
+        (
+            uint80 roundId,
+            int256 price,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        
+        // Get decimals to format the price correctly
+        uint8 decimals = priceFeed.decimals();
+        
+        // Log all the relevant information
+        console2.log("WTI/USD Price Feed Data:");
+        console2.log("Round ID:", roundId);
+        console2.log("Price (raw):", uint256(price));
+        console2.log("Price (USD):", uint256(price) / 10**decimals);
+        console2.log("Last Updated:", updatedAt);
+        
+        // Make sure we got a valid price
+        require(price > 0, "Invalid price");
+        require(updatedAt > 0, "Round not complete");
+        require(answeredInRound >= roundId, "Stale price");
     }
 } 
